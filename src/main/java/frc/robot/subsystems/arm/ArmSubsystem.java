6f7eacceba4def4f281f1bba0f35dee62eca6333
JC Baghdan <joeysamy21308@gmail.com>
2025-10-23 09:59:52 -0400

    arm subsystem scaffolding

--- src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.arm;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.ElevatorConstants;
+
 public class ArmSubsystem {
-    
+     private final SparkMax twinRollerAMotor =
+        new SparkMax(ArmConstants.ARM_ROLLER_MOTORID_A, MotorType.kBrushless);
+    private final SparkMax twinRollerBMotor =
+        new SparkMax(ArmConstants.ARM_ROLLER_MOTORID_B, MotorType.kBrushless);
+    private final SparkMax armArticulationMotor = new SparkMax(ArmConstants.ARM_ARTICULATE_MOTORID, MotorType.kBrushless);
+
 }
