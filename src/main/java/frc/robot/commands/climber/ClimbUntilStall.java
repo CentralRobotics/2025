3dfb99d47470b0186b14d509594942c283a173c4
JC Baghdan <joeysamy21308@gmail.com>
2025-10-24 21:17:48 -0400

    temporarily nuke climber

--- src/main/java/frc/robot/commands/climber/ClimbUntilStall.java
+++ src/main/java/frc/robot/commands/climber/ClimbUntilStall.java
@@ -1,30 +1,30 @@
-package frc.robot.commands.climber;
+// package frc.robot.commands.climber;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.climber.ClimberSubsystem;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.climber.ClimberSubsystem;
 
-public class ClimbUntilStall extends Command {
-  private final ClimberSubsystem climber;
+// public class ClimbUntilStall extends Command {
+//   private final ClimberSubsystem climber;
 
-  public ClimbUntilStall(ClimberSubsystem climber) {
-    this.climber = climber;
-    addRequirements(climber);
-  }
+//   public ClimbUntilStall(ClimberSubsystem climber) {
+//     this.climber = climber;
+//     addRequirements(climber);
+//   }
 
-  @Override
-  public void initialize() {
-    climber.engageRatchet();  // pull against
-    climber.climbUp();
-  }
+//   @Override
+//   public void initialize() {
+//     climber.engageRatchet();  // pull against
+//     climber.climbUp();
+//   }
 
-  @Override
-  public boolean isFinished() {
-    return climber.isStalled();  // hit current threshold briefly
-  }
+//   @Override
+//   public boolean isFinished() {
+//     return climber.isStalled();  // hit current threshold briefly
+//   }
 
-  @Override
-  public void end(boolean interrupted) {
-    climber.stop();
-    climber.engageRatchet(); // holding
-  }
-}
+//   @Override
+//   public void end(boolean interrupted) {
+//     climber.stop();
+//     climber.engageRatchet(); // holding
+//   }
+// }
